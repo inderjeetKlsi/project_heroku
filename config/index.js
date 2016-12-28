31f3b3096d718f44471a9097180b7cf39a798aa7
Inderjeet Singh <inderjeet@whiz-solutions.com>
2016-12-28 15:28:51 +0530

    Procfile

--- config/index.js
+++ config/index.js
@@ -1,8 +1,17 @@
+//   exports.conn ={
+//   user: 'postgres', //env var: PGUSER
+//   database: 'anova', //env var: PGDATABASE
+//   password: 'password', //env var: PGPASSWORD
+//   host: 'localhost', // Server hosting the postgres database
+//   port: 5432, //env var: PGPORT
+//   max: 10, // max number of clients in the pool
+//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+// };
   exports.conn ={
-  user: 'postgres', //env var: PGUSER
-  database: 'anova', //env var: PGDATABASE
-  password: 'password', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
+  user: 'ywsuwupeyhxubp', //env var: PGUSER
+  database: 'dbkovjbnu7sooe', //env var: PGDATABASE
+  password: 'fc368a90790d171c2db92371f1a5e695370f8d6d13187b0554c5cb598b4099f8', //env var: PGPASSWORD
+  host: 'ec2-54-235-112-37.compute-1.amazonaws.com', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express		=	require('express');
 var session		=	require('express-session');
-var bodyParser  = 	require('body-parser');
 var http 		= 	require("http");
 var pg 			= 	require('pg');
 var bodyParser 	= 	require('body-parser');
@@ -8,193 +7,101 @@ var cors 		= 	require('cors');
 var config 		= 	require("./config");
 var app			=	express();
 
+var __SESSION_KEY__ = 'selfEdu@2017';
+
 
 // Please Clear Following lines of codes
-app.use(cors({origin: 'http://localhost'}));
+app.use(cors({origin: 'https://intense-bayou-87341.herokuapp.com'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-app.use(session({secret: 'adakjfhksjfhkjsk',saveUninitialized: true,resave: true}));
-var pool = new pg.Pool(config.conn);
-console.log('pg configured... ');
-// Clear Above Lines
 
-var sess;
+app.engine('html', require('ejs').renderFile);
+// app.use(session({secret: __SESSION_KEY__,saveUninitialized: true,resave: true}));
+var pool = new pg.Pool(config.conn);
+
+
+pg.defaults.ssl = true;
+
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+});
+
+var user_session;
 var data = "";
 var htmldata = "";
 var Result=[];
 var customerDetailAry=[];
-
-pool.connect(function(err, client, done) {
-    console.log('connecting to db'+ client);
-   
-  if(err) {
-    return console.error('error fetching client from pool', err);
-  }
-  var query = client.query("SELECT * FROM customer.tbl_customer");
-  query.on('row', (row) => {
-      Result.push(row);
-    });
-});
-
-pool.on('error', function (err, client) {
- 
-  console.error('idle client error', err.message, err.stack)
-})
-
 
 app.listen(3000,function(){
 	console.log("App Started on PORT 3000");
 });
 
 app.get('/',function(req,res){
-	sess = req.session;
-	if(sess.email)
-	{
-		res.redirect('/home');
-	}
-	else{
-	res.render('login.html');
-	}
+	res.render('user_main.html');
 });
 
-app.post('/login',function(req,res){
-	sess=req.session;	
-	sess.email=req.body.email;
-	sess.pwd = req.body.pass;
-	res.end('done');
-	
+app.get('/forgotPassword',function(req,res){
+	res.render('forgot_password.html');
 });
+
 app.get('/login',function(req,res){
 	res.render('login.html');
-	res.end();
-	
 });
 
-app.get('/home',function(req,res){
-	sess=req.session;
-	if(sess.email)	
-	{
-		res.render('home.html');
-	}
-	else
-	{
-		res.redirect('/');
-	}
+app.get('/signup',function(req,res){
+	res.render('signup.html');
+});
 
-});
-app.get('/dashboard',function(req,res){
-	sess=req.session;
-	if(sess.email)	
-		res.render('home.html');
-	else
-		res.redirect('/');
-});
-app.get('/addCustomer',function(req,res){
-	sess=req.session;
-	if(sess.email){	
-		console.log(sess);
-		
-		if(sess.idCustomer>0){
+
+app.post('/signup',function (req, res) {
+	var szEmail = req.body.szEmail;
+	var szPassword = req.body.szPassword;
+
+	var columns = '"szEmail","szPassword"';
+	var selectColumn = '"szEmail"';
+	if(szEmail != ''){
+
+		var existingEmail = [];
+		var responseAry = [];
 			pool.connect(function(err, client, done) {
-				console.log('connecting to db'+ client);
-			
 			if(err) {
 				return console.error('error fetching client from pool', err);
 			}
-			var query = client.query("SELECT * FROM customer.tbl_customer where id="+ sess.idCustomer );
+
+			var query = client.query("SELECT count(id) as emailExists FROM users.tbl_user where "+selectColumn+"='"+szEmail+"'");
+			// console.log(query);
 			query.on('row', (row) => {
-				customerDetailAry.push(row);
+				console.log(row);
+					existingEmail.push(row);
 				});
+				console.log(existingEmail);
+
+				if(existingEmail == true){
+					responseAry[0] = "ERROR";
+					responseAry[1] = 'This email address is already registered.';
+				}
+				else{
+					// var inserQuery = "INSERT INTO users.tbl_user("+columns+") values('"
+					// 		+ szEmail + "','" 
+					// 		+ szPassword +
+					// 		"')";
+					// 		console.log(inserQuery);
+					// 	client.query(inserQuery);
+					// 	responseAry[0] = "SUCCESS";
+				}
+				res.send(responseAry);
 			});
 		}
-
-		res.render('addCustomer.html');
-	}
-	else
-		res.redirect('/');
-		
-});
-app.get('/customerList',function(req,res){
-	sess=req.session;
-	if(sess.email)	
-		res.render('customerList.html');
-	else
-		res.redirect('/');
-		
+     	
 });
 
-app.get("/customerListing", function (req, res) {
-     res.header('Content-Type', 'text/json');
-     var data = JSON.stringify(Result)
-     res.send(data);
-});
-
-app.get('/logout',function(req,res){
-	
-	req.session.destroy(function(err){
-		if(err){
-			console.log(err);
-		}
-		else
-		{
-			console.log('destroyed');
-			res.render('login.html');
-		}
-	});
-
-});
-
-
-app.post("/addCustomerDetails", function (req, res) {
-    console.log("Add Customer ");
-    console.log(req.body.szEmail);
-    console.log(req.body.szFirstName);
-    console.log(req.body.szLastName);
-	console.log(req.body.szGender);
-    console.log(req.body.szCountry);
-	console.log(req.body.szDescription);
-	
-	var szFirstName = req.body.szFirstName;
-	var szLastName = req.body.szLastName;
-	var szCountry = req.body.szCountry;
-	var szDescription = req.body.szDescription;
-	var szGender = req.body.szGender;
-	var szEmail = req.body.szEmail;
-   var columns = '"szFirstName","szLastName","szCountry","szDescription","szGender","szEmail"';
-    pool.connect(function(err, client, done) {
-    var inserQuery = "INSERT INTO customer.tbl_customer("+columns+") values('"
-					+ szFirstName + "','" 
-					+ szLastName + "','" 
-					+ szCountry + "','" 
-					+ szDescription + "','" 
-					+ szGender + "','" 
-					+ szEmail +
-					"')";
-    client.query(inserQuery);
-	console.log(inserQuery);
-
-     Result = [];
-     var query = client.query("SELECT * FROM customer.tbl_customer");
-     query.on('row', (row) => {
-       Result.push(row);
-     });
-    });  
-     res.send('Inserted');
-});
-
-app.post("/editCustomer", function (req, res) {
-	console.log("Customer id : - ",req.body.idCustomer);
-	var idCustomer = req.body.idCustomer;
-	sess = req.session;
-	if(idCustomer>0){
-		sess.idCustomer=idCustomer;
-		res.end('SUCCESS');
-	}
-	else{
-		res.end('Error : Invalid Customer Id.');
-	}
+app.get('/profile',function(req,res){
+	res.render('profile.html');
+	res.end();
 });
