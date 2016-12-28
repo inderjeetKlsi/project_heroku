128dbb1494accf3e706ce33a2801d53a8bbc927d
Inderjeet Singh <inderjeet@whiz-solutions.com>
2016-12-28 17:15:41 +0530

    Now

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ app.get('/cool', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+console.log('Node app is running on port', app.get('port'))
+});
 
 app.get('/cool', function(request, response) {
   response.send(cool());
