c37731f1fd01661d3e04c9aa343c22ab800c9541
Inderjeet Singh <inderjeet@whiz-solutions.com>
2016-12-28 17:11:22 +0530

    New Index

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
@@ -21,28 +20,7 @@ app.get('/cool', function(request, response) {
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
-=======
-var cool = require('cool-ascii-faces');
-var express = require('express');
-var app = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index')
-});
 
 app.get('/cool', function(request, response) {
   response.send(cool());
-});
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
->>>>>>> a5eebe89b84165a0fd881fff9dade08eb59fa863
 });
