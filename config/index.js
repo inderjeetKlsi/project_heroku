3c6aebcd99f97c4ce6451e8784e8c1228b5b31f6
Inderjeet Singh <inderjeet@whiz-solutions.com>
2017-01-12 12:04:00 +0530

    Procfile

--- config/index.js
+++ config/index.js
@@ -7,3 +7,13 @@
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 };
+
+// exports.conn ={
+//   user: 'u9go1trr7iaqk9', //env var: PGUSER
+//   database: 'dep7lmd20r322q', //env var: PGDATABASE
+//   password: 'pfda4b915345920e9079e60b001144cd9f662b5cbcdefe93e64ce7a84a4276472', //env var: PGPASSWORD
+//   host: 'ec2-52-199-132-95.ap-northeast-1.compute.amazonaws.com', // Server hosting the postgres database
+//   port: 5432, //env var: PGPORT
+//   max: 10, // max number of clients in the pool
+//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+// };
