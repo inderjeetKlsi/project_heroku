38ff7c49ea1531bba5f3226619a4881a438aa073
Inderjeet Singh <inderjeet@whiz-solutions.com>
2017-01-12 15:57:24 +0530

    Jan 12

--- config/index.js
+++ config/index.js
@@ -1,18 +1,8 @@
-//   exports.conn ={
-//   user: 'ywsuwupeyhxubp', //env var: PGUSER
-//   database: 'dbkovjbnu7sooe', //env var: PGDATABASE
-//   password: 'fc368a90790d171c2db92371f1a5e695370f8d6d13187b0554c5cb598b4099f8', //env var: PGPASSWORD
-//   host: 'ec2-54-235-112-37.compute-1.amazonaws.com', // Server hosting the postgres database
-//   port: 5432, //env var: PGPORT
-//   max: 10, // max number of clients in the pool
-//   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
-// };
-
-exports.conn ={
-  user: 'u9go1trr7iaqk9', //env var: PGUSER
-  database: 'dep7lmd20r322q', //env var: PGDATABASE
-  password: 'pfda4b915345920e9079e60b001144cd9f662b5cbcdefe93e64ce7a84a4276472', //env var: PGPASSWORD
-  host: 'ec2-52-199-132-95.ap-northeast-1.compute.amazonaws.com', // Server hosting the postgres database
+  exports.conn ={
+  user: 'ywsuwupeyhxubp', //env var: PGUSER
+  database: 'dbkovjbnu7sooe', //env var: PGDATABASE
+  password: 'fc368a90790d171c2db92371f1a5e695370f8d6d13187b0554c5cb598b4099f8', //env var: PGPASSWORD
+  host: 'ec2-54-235-112-37.compute-1.amazonaws.com', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
